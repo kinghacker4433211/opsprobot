df7505445f0e0f30bd5a36db2ea7cb45714ce6dc
Glitch (yosif11-probot-btroliev2) <none>
2020-09-15 12:35:02 +0000

    🐜🚁 Checkpoint ./index.js:6310492/7

--- index.js
+++ index.js
@@ -1552,7 +1552,7 @@ client.on("message", message => {
   if (message.content === prefix + "image") {
     const embed = new Discord.RichEmbed()
 
-      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
+      .setTitle(`This is  ** ${message.guild.name} server **  Photo !`)
       .setAuthor(message.author.username, message.guild.iconrURL)
       .setColor(0x164fe3)
       .setImage(message.guild.iconURL)
